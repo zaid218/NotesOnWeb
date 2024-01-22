24c679c278436d09933945a210ceb6b5f4468f6e
Mohd ZAID KHAN <zaidk9620@gmail.com>
2024-01-22 12:34:23 +0530

    updated the backend deployed

--- frontend/src/apiconfig.js
+++ frontend/src/apiconfig.js
@@ -1,4 +1,5 @@
 // apiConfig.js
 const baseUrl = "https://mindful-gurukul.onrender.com/api/";
-const host = "http://localhost:5000"
+//const host = "http://localhost:5000"
+const host ="https://notesonweb-sw15.onrender.com"
 export { baseUrl,host };
